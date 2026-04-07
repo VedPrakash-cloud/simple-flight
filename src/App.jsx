bf609d4babe5c33b8a43c479b24816b2cb840e46
VedPrakash-cloud <rocky007ved@gmail.com>
2026-04-07 09:50:52 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import FlightCard from "./card";
+
+function App() {
+  const [data, setData] = useState('');
+  const [flights, setFlights] = useState([]);
+
+  const handleInput = ()=>{
+    const rows = data.split('\n').map(r => r.trim()).filter(l => l !== "");
+    let itinerary = [];
+    let currentLeg = null;
+
+    rows.forEach((line, idx)=>{
+      if(line.match(/(Flight\s\d+|Departing\sflight|Returning\sflight)/i)){
+        if(currentLeg) itinerary.push(currentLeg);
+          currentLeg={
+          header: line,
+          date: rows[idx+1],
+          segments:[],
+          totalEmmissions: ""
+        };
+      }else if(/\d{1,2}:\d{2}\s?[AP]M/i.test(line)){
+        const lastItem = currentLeg?.segments[currentLeg.segments.length-1];
+        
+        if(!lastItem || lastItem.type === 'LAYOVER' || lastItem.arrival){
+          currentLeg.segments.push({
+            type:"FLIGHT",
+            departure: line,
+            duration:rows[idx + 1],
+            arrival: "",
+            airline: "",
+            class: ""
+          });
+        } else{
+          lastItem.arrival = line;
+        }
+      }
+      else if(/(Economy|Business|First|Premium)/i.test(line)){
+        const currentFlight = currentLeg?.segments.find(s => s.type === 'FLIGHT' && !s.class);
+        if(currentFlight){
+          currentFlight.airline = rows[idx - 1];
+          currentFlight.class = line;
+          currentFlight.flightNo = rows[idx + 1];
+        }
+      }
+      else if(line.toLowerCase().includes('layover')){
+        currentLeg?.segments.push({type: 'LAYOVER', content:line});
+      }
+    });
+
+    if(currentLeg) itinerary.push(currentLeg);
+
+    setFlights(itinerary);
+    setData("");
+  }
+
+  return (
+    <div className="p-10">
+      <div>
+        <textarea
+          type="text"
+          value={data}
+          placeholder="Paste flight from google flights..."
+          className="rounded-lg w-full h-48 outline-0 shadow-sm shadow-gray-500/30 inset-shadow-2xs inset-shadow-gray-500/30 p-5"
+          onChange={(e) => setData(e.target.value)}
+        />
+        <input
+          type="submit"
+          value="Submit"
+          className="rounded-lg px-4 py-1 font-semibold cursor-pointer active:scale-95 inset-shadow-sm inset-shadow-gray-500/30 shadow-sm shadow-gray-500/30"
+          onClick={handleInput}
+        />
+      </div>
+      <div className="my-5">
+        <FlightCard itinerary={flights}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/card.jsx
@@ -0,0 +1,32 @@
+export default function FlightCard({ itinerary }) {    
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center">Flight Details</h1>
+      {itinerary.map((item, idx) => (
+        <div key={idx} className="my-6 border border-gray-400/20 rounded-lg px-2 py-5">
+          <h1 className="font-semibold text-2xl mb-5">
+            {item.header} {item.date}
+          </h1>
+          <div className="px-5">
+            {item.segments.map((flight, Flightidx) => (
+              <div key={Flightidx}>
+                {flight.type === "FLIGHT" ? (
+                  <div className="rounded-lg p-5 inset-shadow-sm inset-shadow-mauve-500/30 shadow-sm shadow-pink-500/30">
+                    <p className="font-semibold py-2 text-xl">
+                      {flight.departure.replace(/([AP]M)([A-Z])/i, "$1 $2")}
+                    </p>
+                    <span className="text-base font-semibold px-2 text-gray-500/50">{flight.duration}</span>
+                    <p className="font-semibold pt-2 text-xl">{flight.arrival.replace(/([AP]M)([A-Z])/i,"$1 $2")}</p>
+                    <p className="text-xs font-semibold px-2 text-gray-500/50">{flight.airline} {flight.class} {flight.flightNo.replace(/(\w+)([A-Z0-9][A-Z]\s\d+)/i,"$1 $2")}</p>
+                  </div>
+                ) : (
+                <p className="text-gray-500/50 px-5 my-5 font-semibold">{flight.content.replace(/(layover)([A-Z])/i,"$1 $2")}</p>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
